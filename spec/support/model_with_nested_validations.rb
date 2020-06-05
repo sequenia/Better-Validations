712626175c72f074b8fcd9ec17b29977c90a85b5
Петр Базов <petr@sequenia.com>
2020-06-05 18:30:11 +0700

    RSpec refactoring

--- spec/support/model_with_nested_validations.rb
+++ spec/support/model_with_nested_validations.rb
@@ -1,17 +1,31 @@
 shared_examples 'a model with nested validations' do
   describe '#valid?' do
-    context 'when validating nested objects' do
-      it 'should have presence validation in belongs_to object' do
-        object = described_class.new(belongs_to_object_attributes: {})
-                                .tap(&:valid?)
-        expect(object).to error_on(:'belongs_to_object.attribute_three', :blank)
-        expect(object.belongs_to_object).to error_on(:attribute_three, :blank)
+    let(:object) { described_class.new(attributes).tap(&:valid?) }
+
+    context 'with invalid belongs_to' do
+      let(:attributes) do
+        { belongs_to_object_attributes: {} }
       end
 
-      it 'should have presence validation in has_many objects' do
-        object = described_class.new(has_many_objects_attributes: [{}])
-                                .tap(&:valid?)
+      it 'should have presence validation' do
+        expect(object).to error_on(:'belongs_to_object.attribute_three', :blank)
+      end
+
+      it 'should have nested presence validation' do
+        expect(object.belongs_to_object).to error_on(:attribute_three, :blank)
+      end
+    end
+
+    context 'with invalid has_many' do
+      let(:attributes) do
+        { has_many_objects_attributes: [{}] }
+      end
+
+      it 'should have presence validation' do
         expect(object).to error_on(:'has_many_objects.attribute_five', :blank)
+      end
+
+      it 'should have nested presence validation' do
         expect(object.has_many_objects.first).to error_on(
           :attribute_five,
           :blank
